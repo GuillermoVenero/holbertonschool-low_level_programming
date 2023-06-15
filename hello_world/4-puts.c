5157775593277dc5bdcd646885cd1da908aff1e0
GuillermoVenero <veneroguillermo@gmail.com>
2023-06-15 05:23:52 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
+/*
+ * File: 4-puts.c
+ * Auth: Guillermo Venero
+ */
+
 #include <stdio.h>
+
 /**
- * print- Programming is like building a multilingual puzzle
- * return = 0
+ * main - Prints "Programming is like building a multilingual puzzle,followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {

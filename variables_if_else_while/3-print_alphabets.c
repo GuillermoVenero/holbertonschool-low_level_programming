661b9c19ffd245495e23b04009c65b8f9ef7f164
GuillermoVenero <veneroguillermo@gmail.com>
2023-06-16 09:16:00 -0700

    3-print_alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -main function prints the alphabet
+ * Return: 0.
+ */
+
+int main(void)
+	{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}

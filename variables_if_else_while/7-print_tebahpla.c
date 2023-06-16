d26ba3969ab85baeb511f0382755e227b36dbff2
GuillermoVenero <veneroguillermo@gmail.com>
2023-06-16 10:54:52 -0700

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -main function prints alphabet in reverse
+ * Return: 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}

831c185092b9849ab49abf692465ac645cfba564
GuillermoVenero <veneroguillermo@gmail.com>
2023-06-18 13:43:45 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main -main function print _putchar w putchar only
+ * Return: 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
